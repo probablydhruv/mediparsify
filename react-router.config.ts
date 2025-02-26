64ac4e274b50af1b0d0207e9da0faad59132b638
AkshatRamanathan <ramanathan.ak@northeastern.edu>
2025-02-25 21:57:30 -0500

    fix: deployment work

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,4 +3,6 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   appDirectory: "src",
+  prerender: ["/","/about"],
+  ssr: false
 } satisfies Config;
