900d483ced32dd178b1cafe5a5f759eb8e93901b
AkshatRamanathan <ramanathan.ak@northeastern.edu>
2025-04-22 17:16:48 -0400

    fix: vercel

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { type VariantProps, cva } from 'class-variance-authority';
+import { cva } from 'class-variance-authority';
+import type { VariantProps } from 'class-variance-authority';
 import { Loader2 } from 'lucide-react';
 import { cn } from '@/utils/twUtils';
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
-import { type RouteConfig, index, layout, route, } from "@react-router/dev/routes";
+import { index, layout, route, } from "@react-router/dev/routes";
+import type { RouteConfig } from "@react-router/dev/routes";
 
 export default [
     layout("routes/layout.tsx", [
